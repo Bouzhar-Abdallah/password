5760bc70b2b4f155623828001cdff40c361e29d5
Abdallah Bouzhar <abddoullah.bouzhar@gmail.com>
2022-09-11 20:04:01 +0100

    login set

--- password.cpp
+++ password.cpp
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+struct user {
+		int id;
+		char first_name[10];
+		char last_name[10];
+		char login[];
+	//	char* password;
+	}u;
 
 char * passwordV() {
 	int i,a=0,b=0,c=0;
@@ -55,15 +62,65 @@ if (a>0 && b>0 && c>0){
 	system("CLS");
 	goto start;
 	}
-//	char *password = malloc(10);
     return password;
     
 }
 
 
+void singup(){
+	int length=0,j=0;
+	printf("\n\n\n\n\nentrez fotre prenom\n");
+	gets(u.first_name);
+	printf("\n\n\n\n\nentrez fotre nom\n");
+	gets(u.last_name);
+	
+	//writing first name into login
+ 	for (length=0; u.first_name[length] != '\0';++length) {
+    u.login[length] = u.first_name[length];}
+    //adding a '.' between first name and last name into login
+    u.login[length]= '.';
+    length++;
+    
+	//writing last name into login
+	for (j=0; u.last_name[j] != '\0';++j,length++) {
+    u.login[length] = u.last_name[j];}
+    u.login[length]= '\0';
+    puts(u.first_name);
+    puts(u.last_name);
+    puts(u.login);
+	
+}	
 
 int main(){
-
-	printf("%s",passwordV());
-	return 0;
+			int a=0;
+		
+			printf("\n\n\n\n\n1: to sign up\n2: to log-in\n");
+			scanf(" %d",&a);
+			fflush(stdin);
+		while(a!=1 &&a!=2){
+			system("CLS");
+			printf("\n\n\nvieuller entrez 1 ou 2\n\n");
+			printf("1: to sign up\n2: to log-in\n");
+			scanf("%d",&a);fflush(stdin);
+		}
+				if(a==1){
+					system("CLS");
+					singup();
+				}else{
+				//	login();
+				}
+			//	printf("%s",passwordV());
+				return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
